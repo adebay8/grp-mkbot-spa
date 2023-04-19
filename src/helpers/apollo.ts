013d56814990ebea6c9e56865d799d1ddb14df32
Onuchukwu Adebayo <adebayop.o@gmail.com>
2023-04-19 17:22:39 +0100

    fix: resolved build issues

--- src/helpers/apollo.ts
+++ src/helpers/apollo.ts
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
     uri: 'http://127.0.0.1:8000/graphql',
